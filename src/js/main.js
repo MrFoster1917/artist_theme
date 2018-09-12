49aa1aec49221cbdbc6a763d315244685cdbece9
Mr. Foster <mrfoster1917@gmail.com>
2018-09-11 22:41:14 -0500

    inital commit

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.toggle-nav').click(function(){
+        $('nav').toggleClass('active');
+        toggleNavigation($(this), $('nav'));
+    changeLetters($(this)); 
+    });
+    function toggleNavigation(btn, nav) {
+    btn.toggleClass('open');
+    nav.toggleClass('open');
+  }
+
+  function changeLetters(btn) {
+    var m = $('.toggle-nav span.m');
+    var e = $('.toggle-nav span.e');
+    var n = $('.toggle-nav span.n');
+    var u = $('.toggle-nav span.u');
+
+    e.toggleClass('btn-close');
+
+    if(btn.hasClass('open'))
+    {
+      m.text("E");
+      n.text("I");
+      u.text("T");
+    }
+    else
+    {
+      m.text("M");
+      n.text("N");
+      u.text("U");
+    }
+  }
+});
